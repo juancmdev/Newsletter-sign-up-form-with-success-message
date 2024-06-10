09dce6a03aa7f70860eb6d4865e8c22336705e62
juancmdev <juancmorenoj@gmail.com>
2024-06-10 17:16:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const form = document.querySelector("#form");
 
-console.log(form);
+const handleSubmit = (e) => {
+  e.preventDefault(); //Prevent the defaul behaviour
+
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+};
+
+form.addEventListener("submit", handleSubmit);
