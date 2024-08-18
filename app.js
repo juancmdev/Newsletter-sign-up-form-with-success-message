c605d7608f5ce08898b1e062aa7d075dd654d422
juancmdev <juancmorenoj@gmail.com>
2024-08-18 10:23:48 -0500

    validation input email updated

--- app.js
+++ app.js
@@ -8,36 +8,20 @@ const dismissBtn = document.querySelector("#dismissBtn");
 const emailClient = document.querySelector("#emailClient");
 
 emailValue.value = "";
-const myArr = [];
-
-const myFunct = () => {
-  return emailValue.addEventListener("keyup", (e) => {
-    //console.log(emailValue.value);
-    myArr.push(emailValue.value);
-    console.log("MyArray" + myArr[myArr.length - 1]);
-  });
-};
-
-const email = myFunct();
-
-console.log("Myfunc: " + email);
 
 const handleSubmit = (e) => {
   e.preventDefault(); //Prevent the defaul behaviour
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
 
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (emailValue.value.length === 0 || regex.test(email)) {
+  if (emailValue.value.length === 0 || !validationEmail(data.email)) {
     emailValue.style =
       "background-color: rgba(251, 102, 88, 0.2); border: 1px solid rgba(251, 102, 88)";
     emailTextWarning.style.display = "block";
   } else if (emailValue.value.length > 0) {
-    console.log(emailValue);
+    emailValue.style = "background-color: white; border: 1px solid black";
+    emailTextWarning.style.display = "none";
 
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
-
-    console.log(data.email);
     emailClient.innerHTML = data.email;
 
     main.classList.add("display");
@@ -54,5 +38,7 @@ dismissBtn.addEventListener("click", () => {
   emailValue.value = "";
 });
 
-const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-console.log(regex.test(email));
+function validationEmail(email) {
+  const regex = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
+  return regex.test(email);
+}
